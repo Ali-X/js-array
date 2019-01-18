7e741ccb0a808f0ca3ae3e87ffe76f14b60c75ed
Ali-X <alina240895@yandex.ru>
2019-01-18 20:26:33 +0200

    Implement sixth task

---
+++ js/script6.js
@@ -0,0 +1,18 @@
+let arr = [5, 3, 0, 11, 30];
+
+function getMax(arr) {
+  let maxElement = Number.MIN_VALUE;
+
+  for (let i = 0; i < arr.length; i++) {
+    let arrElement = arr[i];
+
+    if (arrElement > maxElement) {
+      maxElement = arrElement;
+    }
+  }
+
+  return maxElement;
+}
+
+console.log("Input array: " + arr);
+console.log("Max element: " + getMax(arr));
