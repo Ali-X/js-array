5271e34c27664353ed53de3dbd5cd124d0485c20
Ali-X <alina240895@yandex.ru>
2019-01-18 19:38:50 +0200

    Implement third task

---
+++ js/script3.js
@@ -0,0 +1,20 @@
+let students = {Anna: 29, Misha: 35, Stepan: 1, Elena: 99};
+
+
+function getBestStudent(students) {
+  let minMark = Number.MIN_VALUE;
+  let bestStudent;
+
+  for (st in students) {
+    let studentMark = students[st];
+
+    if (studentMark > minMark) {
+      minMark = studentMark;
+      bestStudent = st;
+    }
+  }
+
+  return bestStudent;
+}
+
+console.log(getBestStudent(students));
