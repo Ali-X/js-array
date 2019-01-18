7440a5331ad8284aa0374656ed3eb4da988500cd
Ali-X <alina240895@yandex.ru>
2019-01-18 20:23:36 +0200

    Implement fifth task

---
+++ js/script5.js
@@ -0,0 +1,14 @@
+let arr = [5, 3, 0, 11, 30];
+
+function getSumm(arr) {
+  let summ = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    summ += arr[i];
+  }
+
+  return summ;
+}
+
+console.log("Input array: " + arr);
+console.log("Summ: " + getSumm(arr));
